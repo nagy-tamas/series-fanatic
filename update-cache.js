b39108a18c48f6dcf994e9c8d16ab9703d5b0a53
unknown <nagytamasnak@gmail.com>
2015-10-23 13:51:09 +0200

    using promises + functions went to the utils

--- update-cache.js
+++ update-cache.js
@@ -1,79 +1,23 @@
+/////////////////////////////////////////////////////////////////
+// This is a CLI tool to update a seeries' data from wikipedia //
+/////////////////////////////////////////////////////////////////
+
 var db = require('./db/db'),
-  utils = require('./lib/utils'),
-  fs = require('fs'),
-  request = require('request'),
-  cheerio = require('cheerio');
+  utils = require('./lib/utils');
 
 var argv = require('yargs')
   .default('s', 'Family Guy')
-  .argv;
+  .argv,
+  seriesName = argv.s;
 
-var writeJSON = function(fileName, data, cb) {
-  fs.writeFile(fileName, JSON.stringify(data, null, 2), function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      cb();
-    }
+utils.parseWiki(db.findByTitle(seriesName))
+  .then(function(json) {
+    utils.writeJSON('./db/' + utils.getSlugNameFor(seriesName) + '.json', json);
+  })
+  .then(function() {
+    console.log('Everything worked out fine,', utils.getSlugNameFor(seriesName), '\s cache was updated.');
+  })
+  .catch(function(err) {
+    console.error('Some error happened during updating:', seriesName, ', halted. Details:');
+    console.error(err);
   });
-};
-
-var parseWiki = function(series, cb) {
-  var ret = {}, $;
-
-  request(series.url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      $ = cheerio.load(body);
-      var ret = [], season;
-
-      $('h3').each(function(i, el) {
-        var title = $(el).text().trim(), $table, $tds;
-
-        if (title.indexOf('Season ') === 0) {
-          $table = $(el).nextAll('table').eq(0);
-          title = /Season (\d+)+/.exec(title)[1];
-
-          season = {
-            season: title,
-            episodes: []
-          };
-
-          if ($table.length !== 1) {
-            console.error('Something wrong with the page', series, title);
-          }
-
-          $table.find('.vevent').each(function(i, tr) {
-            var $tds = $(tr).find('td'),
-              episodeNo = $tds.eq(0).text().trim(),
-              episode = $tds.eq(1).text().trim(),
-              date = $tds.eq(4).text().trim();
-
-            // removing "" -s around it
-            episode = episode.slice(1, -1);
-
-            // October 4, 2015 (2015-10-04) -> 2015-10-04
-            date = /.\((.*?)\).*/.exec(date)[1];
-            season.episodes.push({
-              no: episodeNo,
-              title: episode,
-              date: date
-            });
-            // console.log(title, episodeNo, date, new Date(date));
-          });
-
-          ret.push(season);
-        }
-      });
-
-      cb(ret);
-    }
-  });
-
-  return '';
-}
-
-parseWiki(db.findByTitle(argv.s), function(json) {
-  writeJSON('./db/' + utils.slugName(argv.s) + '.json', json, function() {
-    console.log('Everything worked out fine.');
-  });
-});
